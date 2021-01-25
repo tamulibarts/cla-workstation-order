6cf7a30b62dba1cef766f7ddaaf2686f8023c756
Zachary Watkins <watkinza@gmail.com>
2021-01-25 08:29:23 -0600

    Add in-progress hook for reacting to changes in post status

--- src/class-wsorder-posttype.php
+++ src/class-wsorder-posttype.php
@@ -29,7 +29,7 @@ class WSOrder_PostType {
 		// Register_post_types.
 		add_action( 'init', array( $this, 'register_post_type' ) );
 		add_action( 'acf/init', array( $this, 'register_custom_fields' ) );
-
+		add_action( 'transition_post_status', array( $this, 'notify_published' ), 10, 3 );
 	}
 
 	/**
@@ -48,7 +48,7 @@ class WSOrder_PostType {
 				'plural'   => 'Orders',
 			),
 			'wsorder',
-			array( 'fiscal-year' ),
+			array(),
 			'dashicons-portfolio',
 			array( 'title' ),
 			array(
@@ -66,5 +66,38 @@ class WSOrder_PostType {
 	 */
 	public function register_custom_fields() {
 		require_once CLA_WORKSTATION_ORDER_DIR_PATH . 'fields/wsorder-fields.php';
+	}
+
+	/**
+	 * Notify users based on their association to the wsorder post and the new status of the post.
+	 *
+	 * @since 0.1.0
+	 * @param string  $new_status The post's new status.
+	 * @param string  $old_status The post's old status.
+	 * @param WP_Post $post       The post object.
+	 * @return void
+	 */
+	public function notify_published( $new_status, $old_status, $post ) {
+
+		if ( 'wsorder' !== $post->post_type ) {
+			return;
+		}
+
+		$message .= serialize( $_POST ); //phpcs:ignore
+		$message .= serialize( $post ); //phpcs:ignore
+
+		$it_rep_user_id       = $_POST['acf']['field_5fff6b46a22af']['field_5fff703a5289f']; //phpcs:ignore
+		$it_rep_user_id_saved = get_post_meta( $post->ID, 'it_rep_status_it_rep' );
+		$message             .= $it_rep_user_id . ' : ' . $it_rep_user_id_saved;
+		wp_mail( 'zwatkins2@tamu.edu', 'order published', $message );
+		if (
+			( 'publish' === $new_status && 'publish' !== $old_status )
+		&& 'wsorder' === $post->post_type
+		) {
+			$message  = serialize( $_GET ); //phpcs:ignore
+			$message .= serialize( $_POST ); //phpcs:ignore
+			$message .= serialize( $post ); //phpcs:ignore
+			wp_mail( 'zwatkins2@tamu.edu', 'order published', $message );
+		}
 	}
 }
