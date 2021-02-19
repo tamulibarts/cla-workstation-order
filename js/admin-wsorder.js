69b43378c8ada4a53045d913626aba9fd7eb1dc8
Zachary Watkins <watkinza@gmail.com>
2021-02-19 13:23:43 -0600

    For Order post type move script to window load event and change body class to current post status selection value

--- js/admin-wsorder.js
+++ js/admin-wsorder.js
@@ -1,7 +1,11 @@
-if ( jQuery('#post-status-select #post_status').val() === 'draft' ) {
-	jQuery('#post-status-select #post_status').val('action_required');
-	jQuery('#post-status-display').html('Action Required');
-}
-jQuery('#post-status-select #post_status option[value="pending"]').remove();
-jQuery('#post-status-select #post_status option[value="draft"]').remove();
-// jQuery('#post-body-content #title').attr('disabled','disabled');
+jQuery(window).on('load', function(){
+	if ( jQuery('#post-status-select #post_status').val() === 'draft'  ) {
+		jQuery('#post-status-select #post_status').val('action_required');
+		jQuery('#post-status-display').html('Action Required');
+	}
+	// jQuery('#post-body-content #title').attr('disabled','disabled');
+	// Show the save button when the work order status is "completed".
+	jQuery('#post-status-select #post_status').on('change', function(){
+		jQuery('body').removeClass('action_required completed returned awaiting_another').addClass(this.value);
+	});
+});
