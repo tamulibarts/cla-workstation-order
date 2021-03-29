9adb3834d78833822ba307d119a2525dccbcbb20
Zachary Watkins <watkinza@gmail.com>
2021-03-29 15:49:36 -0500

    Make disallowed products and bundles fields return post ids instead of post objects

--- fields/department-fields.php
+++ fields/department-fields.php
@@ -8,95 +8,93 @@
  * @subpackage cla-workstation-order/fields
  */
 
-if ( function_exists( 'acf_add_local_field_group' ) ) :
+if( function_exists('acf_add_local_field_group') ):
 
-	acf_add_local_field_group(
+acf_add_local_field_group(array(
+	'key' => 'group_6000774a01105',
+	'title' => 'Department Fields',
+	'fields' => array(
 		array(
-			'key'                   => 'group_6000774a01105',
-			'title'                 => 'Department Fields',
-			'fields'                => array(
-				array(
-					'key'               => 'field_6000774e10970',
-					'label'             => 'Abbreviation',
-					'name'              => 'abbreviation',
-					'type'              => 'text',
-					'instructions'      => '',
-					'required'          => 0,
-					'conditional_logic' => 0,
-					'wrapper'           => array(
-						'width' => '',
-						'class' => '',
-						'id'    => '',
-					),
-					'default_value'     => '',
-					'placeholder'       => '',
-					'prepend'           => '',
-					'append'            => '',
-					'maxlength'         => 12,
-				),
-				array(
-					'key'               => 'field_6000776a10971',
-					'label'             => 'Hidden Products',
-					'name'              => 'hidden_products',
-					'type'              => 'post_object',
-					'instructions'      => '',
-					'required'          => 0,
-					'conditional_logic' => 0,
-					'wrapper'           => array(
-						'width' => '',
-						'class' => '',
-						'id'    => '',
-					),
-					'post_type'         => array(
-						0 => 'product',
-					),
-					'taxonomy'          => '',
-					'allow_null'        => 0,
-					'multiple'          => 1,
-					'return_format'     => 'object',
-					'ui'                => 1,
-				),
-				array(
-					'key'               => 'field_6000a59b5f961',
-					'label'             => 'Hidden Bundles',
-					'name'              => 'hidden_bundles',
-					'type'              => 'post_object',
-					'instructions'      => '',
-					'required'          => 0,
-					'conditional_logic' => 0,
-					'wrapper'           => array(
-						'width' => '',
-						'class' => '',
-						'id'    => '',
-					),
-					'post_type'         => array(
-						0 => 'bundle',
-					),
-					'taxonomy'          => '',
-					'allow_null'        => 0,
-					'multiple'          => 0,
-					'return_format'     => 'object',
-					'ui'                => 1,
-				),
+			'key' => 'field_6000774e10970',
+			'label' => 'Abbreviation',
+			'name' => 'abbreviation',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
 			),
-			'location'              => array(
-				array(
-					array(
-						'param'    => 'post_type',
-						'operator' => '==',
-						'value'    => 'department',
-					),
-				),
+			'default_value' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'maxlength' => 12,
+		),
+		array(
+			'key' => 'field_6000776a10971',
+			'label' => 'Hidden Products',
+			'name' => 'hidden_products',
+			'type' => 'post_object',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
 			),
-			'menu_order'            => 0,
-			'position'              => 'normal',
-			'style'                 => 'default',
-			'label_placement'       => 'top',
-			'instruction_placement' => 'label',
-			'hide_on_screen'        => '',
-			'active'                => true,
-			'description'           => '',
-		)
-	);
+			'post_type' => array(
+				0 => 'product',
+			),
+			'taxonomy' => '',
+			'allow_null' => 0,
+			'multiple' => 1,
+			'return_format' => 'id',
+			'ui' => 1,
+		),
+		array(
+			'key' => 'field_6000a59b5f961',
+			'label' => 'Hidden Bundles',
+			'name' => 'hidden_bundles',
+			'type' => 'post_object',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'post_type' => array(
+				0 => 'bundle',
+			),
+			'taxonomy' => '',
+			'allow_null' => 0,
+			'multiple' => 0,
+			'return_format' => 'id',
+			'ui' => 1,
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'department',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+));
 
 endif;
