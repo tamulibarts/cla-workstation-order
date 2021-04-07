4d942960f706ee83c1e66b5fd3b052206a1730bf
Zachary Watkins <watkinza@gmail.com>
2021-04-07 16:26:47 -0500

    Implement dropdown post filters for product category and program

--- src/class-product-posttype.php
+++ src/class-product-posttype.php
@@ -32,6 +32,10 @@ class Product_PostType {
 		add_filter( 'manage_product_posts_columns', array( $this, 'product_filter_posts_columns' ) );
 		add_action( 'manage_product_posts_custom_column', array( $this, 'product_column' ), 10, 2 );
 		add_action( 'pre_get_posts', array( $this, 'product_posts_orderby' ) );
+		add_action( 'restrict_manage_posts', array( $this, 'add_admin_post_program_filter' ), 10 );
+		add_filter( 'parse_query', array( $this, 'parse_query_program_filter' ), 10);
+		add_action( 'restrict_manage_posts', array( $this, 'add_admin_post_category_filter' ), 10 );
+
 
 	}
 
@@ -134,5 +138,116 @@ class Product_PostType {
 			$query->set( 'meta_key', 'program' );
 			$query->set( 'meta_type', 'numeric' );
 		}
+	}
+
+	/**
+	 * Render filters for Product post meta on the bulk posts page.
+	 *
+	 * @param string $post_type The post type.
+	 *
+	 * @return void
+	 */
+	public function add_admin_post_program_filter( $post_type ) {
+
+		if ( 'product' !== $post_type ){
+		  return; //filter your post
+		}
+		$selected = '';
+		$request_attr = 'program';
+		if ( isset( $_REQUEST[$request_attr] ) ) {
+		  $selected = $_REQUEST[$request_attr];
+		}
+		//get unique values of the meta field to filer by.
+		$meta_key = 'program';
+		global $wpdb;
+		$results = $wpdb->get_col(
+			$wpdb->prepare( "
+				SELECT DISTINCT pm.meta_value FROM {$wpdb->postmeta} pm
+				LEFT JOIN {$wpdb->posts} p ON p.ID = pm.post_id
+				WHERE pm.meta_key = '%s'
+				AND p.post_status IN ('publish', 'draft')
+				ORDER BY pm.meta_value",
+				$meta_key
+			)
+		);
+		//build a custom dropdown list of values to filter by
+		echo '<select id="program" name="program">';
+		echo '<option value="0">' . __( 'Show all Programs', 'cla-workstation-order' ) . ' </option>';
+		foreach( $results as $program ) {
+			if ( ! empty( $program ) ) {
+				$select = ($program == $selected) ? ' selected="selected"':'';
+				echo '<option value="'.$program.'"'.$select.'>' . get_the_title( $program ) . ' </option>';
+			}
+		}
+		echo '</select>';
+
+  }
+
+	/**
+	 * Modify the post query based on custom product filter dropdown selections.
+	 *
+	 * @param WP_Query $query The query object.
+	 *
+	 * @return WP_Query
+	 */
+	public function parse_query_program_filter( $query ){
+
+		//modify the query only if it admin and main query.
+		if( !(is_admin() AND $query->is_main_query()) ){
+			return $query;
+		}
+		//we want to modify the query for the targeted custom post and filter option
+		if( !('product' === $query->query['post_type'] AND isset($_REQUEST['program']) ) ){
+			return $query;
+		}
+		//for the default value of our filter no modification is required
+		if(0 == $_REQUEST['program']){
+			return $query;
+		}
+		//modify the query_vars.
+		$query->query_vars['name'] = '';
+		$meta_query = $query->get( 'meta_query' );
+		if ( ! is_array( $meta_query ) ) {
+			$meta_query = array();
+		}
+		if ( ! empty( $_REQUEST['program'] ) ) {
+			$meta_query[] = array(
+				'key'	 => 'program',
+				'value' => $_REQUEST['program'],
+			);
+		}
+		$query->set( 'meta_query', $meta_query );
+		return $query;
+
+	}
+
+	/**
+	 * Render filters for Product post meta on the bulk posts page.
+	 *
+	 * @param string $post_type The post type.
+	 *
+	 * @return void
+	 */
+	public function add_admin_post_category_filter( $post_type ) {
+
+		if ( 'product' !== $post_type ){
+			return; //filter your post
+		}
+		$slug     = 'product-category';
+		$taxonomy = get_taxonomy( $slug );
+		$selected = '';
+		// if the current page is already filtered, get the selected term slug
+		$selected = isset( $_REQUEST[ $slug ] ) ? $_REQUEST[ $slug ] : '';
+		// render a dropdown for this taxonomy's terms
+		wp_dropdown_categories( array(
+			'show_option_all' =>  $taxonomy->labels->all_items,
+			'taxonomy'        =>  $slug,
+			'name'            =>  $slug,
+			'orderby'         =>  'name',
+			'value_field'     =>  'slug',
+			'selected'        =>  $selected,
+			'hierarchical'    =>  true,
+		) );
+
 	}
 }
