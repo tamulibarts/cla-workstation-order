881bbd1d8e5735537951d15b73711606d1b97301
Zachary Watkins <watkinza@gmail.com>
2021-10-07 07:11:37 -0500

    Correcting backup location

--- backup/backup.sh
+++ backup/backup.sh
@@ -2,4 +2,4 @@ today=$(date +"%Y-%m-%d-%H%M%S")
 install=wsorder
 filename=wsorder
 domain="ssh.wpengine.net"
-$install@$install.$domain "wp db export - " > backup/{$filename}-{$today}.sql
+ssh $install@$install.$domain "wp db export - " > backup/$filename-$today.sql
