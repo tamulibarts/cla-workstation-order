e2fdf55c81cb52082f0fb2effaa805da9bcc3869
Zach Watkins <watkinza@gmail.com>
2021-10-11 11:24:23 -0500

    Update backup.sh
    Fixed environmental variable pointers in the backup shell file

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,5 +1,5 @@
 today=$(date +"%Y-%m-%d-%H%M%S")
-install=wsorder
-filename=wsorder
+install=$wsorder
+filename=$wsorder
 domain="ssh.wpengine.net"
 ssh $install@$install.$domain "wp db export - " > backup/$filename-$today.sql
