e30f85e23b7a2d1fd7c6c6c26ad69350e0b64b33
Zachary Watkins <zwatkins2@tamu.edu>
2021-10-12 15:20:18 -0500

    Attempt to disable Joseph's CAS plugin when on my local development environment but in a way that allows us to use WP Engine's APIs to connect to the server

--- src/class-legacy-support.php
+++ src/class-legacy-support.php
@@ -28,6 +28,37 @@ class Legacy_Support {
 		add_action( 'init', array( $this, 'uncasify_user_switching' ) );
 		add_action( 'admin_init', array( $this, 'uncasify_user_switching' ) );
 
+		// Disable Joseph's CAS plugin when developing locally and not logged in yet.
+		if (
+			isset( $_GET['localwp_auto_login'] )
+			&& defined( 'WP_ENVIRONMENT_TYPE' )
+			&& 'local' === WP_ENVIRONMENT_TYPE
+			&& defined( 'WP_ADMIN' )
+			&& (
+				! defined( 'DOING_AJAX' )
+				|| false === boolval( DOING_AJAX )
+			)
+			&& (
+				! defined( 'DOING_CRON' )
+				|| false === boolval( DOING_CRON )
+			)
+		) {
+			add_action( 'plugins_loaded', function(){
+				error_log('removing filters and actions');
+				remove_filter('authenticate', array('IUCASAuthentication', 'authenticate_filter'), 30, 3 );
+				remove_filter('login_redirect', array('IUCASAuthentication', 'remove_ticket_from_redirect') );
+				remove_action('wp_logout', array('IUCASAuthentication', 'logout'));
+				remove_filter('show_password_fields', array('IUCASAuthentication', 'show_password_fields'));
+				remove_action('check_passwords', array('IUCASAuthentication', 'check_passwords'), 10, 3);
+				remove_filter('login_url', array('IUCASAuthentication', 'casify_login_url'));
+				remove_action( 'init', 'cla_forcelogin' );
+			});
+		} else {
+			error_log( 'Condition not met. Logging out.' );
+			add_action( 'plugins_loaded', function(){
+				wp_logout();
+			});
+		}
 	}
 
 	/**
